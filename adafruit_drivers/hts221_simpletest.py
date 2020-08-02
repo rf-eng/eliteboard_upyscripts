fa668553d1ae57c6e34cde00a0a35de73cfe1e96
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2020-08-02 12:57:11 +0200

    fix i2c initialization for our modified busio implementation

--- adafruit_drivers/hts221_simpletest.py
+++ adafruit_drivers/hts221_simpletest.py
@@ -1,9 +1,8 @@
 import time
-import board
 import busio
 import adafruit_hts221
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C()
 hts = adafruit_hts221.HTS221(i2c)
 while True:
     print("Relative Humidity: %.2f %% rH" % hts.relative_humidity)
--- adafruit_drivers/lsm6ds_full_test.py
+++ adafruit_drivers/lsm6ds_full_test.py
@@ -1,11 +1,10 @@
 import time
-import board
 import busio
 
 # pylint:disable=no-member,unused-import
 from adafruit_lsm6ds import LSM6DS33, LSM6DSOX, ISM330DHCT, Rate, AccelRange, GyroRange
 
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C()
 
 # sensor = LSM6DS33(i2c)
 sensor = LSM6DSOX(i2c, address=107)
