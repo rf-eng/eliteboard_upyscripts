a84a4cdda7b98a3f33d06ae62635fe1ad304b3c8
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2020-10-05 13:47:54 +0200

    add more i2c devices and fix addresses

--- i2c_scan.py
+++ i2c_scan.py
@@ -5,20 +5,28 @@ adr_list = i2c.scan()
 for adr in adr_list:
     if '0x30' in hex(adr<<1):
         print('I2C One Wire Bridge at {}/{}'.format(hex(adr<<1), hex(adr)))
+    elif '0x50' in hex(adr<<1):
+        print('USB-C Controller STUSB at {}/{}'.format(hex(adr<<1), hex(adr)))
     elif '0x34' in hex(adr<<1):
         print('Audio Codec at {}/{}'.format(hex(adr<<1), hex(adr)))
     elif '0x3c' in hex(adr<<1):
         print('3D Magnetometer at {}/{}'.format(hex(adr<<1), hex(adr)))
-    elif '0x8a' in hex(adr<<1):
+    elif '0x62' in hex(adr<<1):
         print('Programmable DC/DC Converter at {}/{}'.format(hex(adr<<1), hex(adr)))
-    elif '0xa0' in hex(adr<<1):
-        print('I2C EEPROM at {}/{}'.format(hex(adr<<1), hex(adr)))
+    elif '0xa4' in hex(adr<<1):
+        print('I2C EEPROM mem array at {}/{}'.format(hex(adr<<1), hex(adr)))
+    elif '0xb4' in hex(adr<<1):
+        print('I2C EEPROM ID page at {}/{}'.format(hex(adr<<1), hex(adr)))
     elif '0xba' in hex(adr<<1):
         print('Barometer at {}/{}'.format(hex(adr<<1), hex(adr)))
     elif '0xbe' in hex(adr<<1):
         print('Relative Humidity + Temperature at {}/{}'.format(hex(adr<<1), hex(adr)))
     elif '0xd6' in hex(adr<<1):
         print('3D Accelerometer + 3D Gyroscope at {}/{}'.format(hex(adr<<1), hex(adr)))
+    elif '0x8a' in hex(adr<<1):
+        print('Digital Power Monitor at {}/{}'.format(hex(adr<<1), hex(adr)))
+    elif '0x70' in hex(adr<<1):
+        print('Port Expander ADC2 at {}/{}'.format(hex(adr<<1), hex(adr)))
     else:
         print('*** Unknown device at {}/{}'.format(hex(adr<<1), hex(adr)))
 
@@ -36,4 +44,5 @@ for adr in adr_list:
 
 # Port Expander ADC2 0x40
 # Digital Power Monitor????
+
 
