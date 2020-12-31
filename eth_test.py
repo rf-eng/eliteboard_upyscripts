13b3bf95d1d72c5309eec8105253055bf1c86d96
Reinhard Feger <47209718+rf-eng@users.noreply.github.com>
2020-12-31 10:58:39 +0100

    add http_get to ethernet test function

--- eth_test.py
+++ eth_test.py
@@ -1,5 +1,19 @@
 import network
 import time
+import socket
+
+def http_get(url):
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 80)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
 
 lan = network.LAN()
 lan.active(True)
@@ -10,3 +24,5 @@ ip_settings = lan.ifconfig()
 print("IP: {}".format(ip_settings[0]))
 print("netmask: {}".format(ip_settings[1]))
 #print(": {}".format(ip_settings[2]))
+
+http_get('http://micropython.org/ks/test.html')
